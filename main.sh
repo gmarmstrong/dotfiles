40243aad2694394fefea75426726494a0fe3cd5a
gmarmstrong <guthrie.armstrong@gmail.com>
2017-08-22 11:22:14 -0400

    Debugging mode for main.sh

--- main.sh
+++ main.sh
@@ -5,26 +5,70 @@
 # TODO Implement logging with variable verbosity
 # TODO Link ssh config
 
+# Set default values
+debug=false
+
+# Check for debug flag
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -d|--debug)
+            debug=true
+            ;;
+        *) # Do something with extra options
+            echo "Unknown option '$key'"
+            ;;
+    esac
+    shift # Shift after checking all cases to get the next option
+done
+
 # Set up apt-get
-sudo apt-get -y -qq update
-sudo apt-get -y -qq upgrade
-sudo apt-get -y -qq autoremove
+if $debug
+then
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+    sudo apt-get -y autoremove
+else
+    sudo apt-get -y -qq update
+    sudo apt-get -y -qq upgrade
+    sudo apt-get -y -qq autoremove
+fi
 
 # Install VirtualBox guest additions
 # FIXME Incomplete
-sudo apt-get -qq install make
-sudo mount -t iso9660 /dev/cdrom /media/cdrom
-cd /media/cdrom
-sudo sh ./VBoxLinuxAdditions.run
+if $debug
+then
+    sudo apt-get -y install make
+    sudo mount -t iso9660 /dev/cdrom /media/cdrom
+    cd /media/cdrom
+    sudo sh ./VBoxLinuxAdditions.run
+else
+    sudo apt-get -y -qq install make
+    sudo mount -t iso9660 /dev/cdrom /media/cdrom # TODO Possibly silence
+    cd /media/cdrom
+    sudo sh ./VBoxLinuxAdditions.run # TODO Possibly silence
+fi
 cd ~
 
 # Install pip and pip3
-sudo apt-get -y -qq install curl
-curl --silent https://bootstrap.pypa.io/get-pip.py | sudo python
-curl --silent https://bootstrap.pypa.io/get-pip.py | sudo python3
+if $debug
+then
+    sudo apt-get -y install curl
+    curl https://bootstrap.pypa.io/get-pip.py | sudo python
+    curl https://bootstrap.pypa.io/get-pip.py | sudo python3
+else
+    sudo apt-get -y -qq install curl
+    curl --silent https://bootstrap.pypa.io/get-pip.py | sudo python
+    curl --silent https://bootstrap.pypa.io/get-pip.py | sudo python3
+fi
 
 # Set up git and GitHub
-sudo apt-get -y -qq install git git-flow git-doc git-email
+if $debug
+then
+    sudo apt-get -y install git git-flow git-doc git-email
+else
+    sudo apt-get -y -qq install git git-flow git-doc git-email
+fi
 read -p "Enter email used for GitHub: " email
 read -p "Enter GitHub username: " github_username
 read -p "Enter a unique name for your GitHub key: " github_keyname
@@ -32,25 +76,55 @@ git config --global user.name "$github_username"
 git config --global user.email "$email"
 
 # Generate key and upload to GitHub
-ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa_github
-curl -u ${github_username} --data '{"title":"'"$github_keyname"'","key":"'"$(cat ~/.ssh/id_rsa_github.pub)"'"}' https://api.github.com/user/keys
+if $debug
+then
+    ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa_github
+    curl -u -${github_username} --data '{"title":"'"$github_keyname"'","key":"'"$(cat ~/.ssh/id_rsa_github.pub)"'"}' https://api.github.com/user/keys
+else
+    ssh-keygen -t -q rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/id_rsa_github
+    curl -s -u ${github_username} --data '{"title":"'"$github_keyname"'","key":"'"$(cat ~/.ssh/id_rsa_github.pub)"'"}' https://api.github.com/user/keys
+fi
 
 # Install vim and vim-plug
-sudo apt-get -y -qq install vim
-mkdir -p ~/.vim/colors
-wget -q -O ~/.vim/colors/Tomorrow.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow.vim
-wget -q -O ~/.vim/colors/Tomorrow-Night.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night.vim
-curl -fLo --silent ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# TODO Use either wget or curl, not both
+if $debug
+then
+    sudo apt-get -y install vim
+    mkdir -p ~/.vim/colors
+    wget -O ~/.vim/colors/Tomorrow.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow.vim
+    wget -O ~/.vim/colors/Tomorrow-Night.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+else
+    sudo apt-get -y -qq install vim
+    mkdir -p ~/.vim/colors
+    wget -q -O ~/.vim/colors/Tomorrow.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow.vim
+    wget -q -O ~/.vim/colors/Tomorrow-Night.vim https://raw.githubusercontent.com/chriskempson/tomorrow-theme/master/vim/colors/Tomorrow-Night.vim
+    curl -fLo --silent ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
 # Get dotfiles
-if curl -s --head https://github.com/${github_username}/dotfiles.git | head -n 1 | grep -q "HTTP/1.[01] [23].."
+if $debug
 then
-    git clone ssh://git@github.com/${github_username}/dotfiles.git
-    if curl -s --head https://github.com/${github_username}/dotfiles/blob/master/vimrc | head -n 1 | grep -q "HTTP/1.[01] [23].."
+    if curl --head https://github.com/${github_username}/dotfiles.git | head -n 1 | grep -q "HTTP/1.[01] [23].."
     then
-        ln -s dotfiles/vimrc ~/.vimrc
+        git clone ssh://git@github.com/${github_username}/dotfiles.git
+        if curl --head https://github.com/${github_username}/dotfiles/blob/master/vimrc | head -n 1 | grep "HTTP/1.[01] [23].."
+        then
+            ln -s dotfiles/vimrc ~/.vimrc
+        fi
+    fi
+else
+    if curl -s --head https://github.com/${github_username}/dotfiles.git | head -n 1 | grep -q "HTTP/1.[01] [23].."
+    then
+        git -q clone ssh://git@github.com/${github_username}/dotfiles.git
+        if curl -s --head https://github.com/${github_username}/dotfiles/blob/master/vimrc | head -n 1 | grep -q "HTTP/1.[01] [23].."
+        then
+            ln -s dotfiles/vimrc ~/.vimrc
+        fi
     fi
 fi
 
@@ -62,7 +136,12 @@ vim +PlugInstall +qall
 touch ~/.hushlogin
 
 # Set up zsh
-sudo apt-get -y -qq install zsh zsh-doc
+if $debug
+then
+    sudo apt-get -y install zsh zsh-doc
+else
+    sudo apt-get -y -qq install zsh zsh-doc
+fi
 
 # Set up oh-my-zsh (this must be the last command of this script)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
