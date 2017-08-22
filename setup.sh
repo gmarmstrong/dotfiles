f19580932e7af986ed6d9f2d9ca83875a8247b1a
gmarmstrong <guthrie.armstrong@gmail.com>
2017-08-22 11:03:50 -0400

    Debugging mode for setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,23 @@
 
 # setup.sh
 
+# Set default values
+debug=false
+
+# Check for debug flag
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case "$key" in
+        -d|--debug)
+            debug=true
+            ;;
+        *) # Do something with extra options
+            echo "Unknown option '$key'"
+            ;;
+    esac
+    shift # Shift after checking all cases to get the next option
+done
+
 # Test Debian
 if ! [ -f "/etc/debian_version" ];
 then
@@ -10,17 +27,39 @@ then
 fi
 
 # Test connection
-if ! ping -q -c 1 google.com
+if $debug
 then
-    echo Internet connection failed, aborting.
-    exit 1
+    if ! ping -c 1 google.com
+    then
+        echo Internet connection failed, aborting.
+        exit 1
+    fi
+else
+    if ! ping -c -q 1 google.com
+    then
+        echo Internet connection failed, aborting.
+        exit 1
+    fi
 fi
 
 # Download post-install scripts
-wget -q https://raw.githubusercontent.com/gmarmstrong/post-install/master/main.sh
-wget -q https://raw.githubusercontent.com/gmarmstrong/post-install/master/postzsh.sh
+if $debug
+then
+    wget https://raw.githubusercontent.com/gmarmstrong/post-install/master/main.sh
+    wget https://raw.githubusercontent.com/gmarmstrong/post-install/master/postzsh.sh
+else
+    wget -q https://raw.githubusercontent.com/gmarmstrong/post-install/master/main.sh
+    wget -q https://raw.githubusercontent.com/gmarmstrong/post-install/master/postzsh.sh
+fi
 
 # Set up sudo
 export user=$(whoami)
-su root -c "apt-get -y -qq install sudo"
-su root -c "adduser $user sudo"
+if $debug
+then
+    su root -c "apt-get -y install sudo"
+    su root -c "adduser $user sudo"
+else
+    su root -c "apt-get -y -qq install sudo"
+    su root -c "adduser $user sudo"
+fi
+
