765814c415d4d03582b6434c8204143b3d7eff1b
gmarmstrong <guthrie.armstrong@gmail.com>
2017-08-22 11:56:28 -0400

    Make .ssh directory

--- main.sh
+++ main.sh
@@ -77,11 +77,13 @@ git config --global user.email "$email"
 # Generate key and upload to GitHub
 if $debug
 then
+    mkdir .ssh
     ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa_github
     curl -u -${github_username} --data '{"title":"'"$github_keyname"'","key":"'"$(cat ~/.ssh/id_rsa_github.pub)"'"}' https://api.github.com/user/keys
 else
+    mkdir .ssh
     ssh-keygen -t -q rsa -b 4096 -C "$email" -f ~/.ssh/id_rsa_github
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/id_rsa_github
