dc4c54cccadb3fe77aed8900689cfa10c5a30aab
gmarmstrong <guthrie.armstrong@gmail.com>
2017-11-16 21:07:51 -0500

    Replace git/config with git/config.sh

---
+++ git/config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Comply with XDG Base Directory Specification
+git_config_dir="$HOME/.config/git"
+
+# Reset Git config directory
+rm -rf "$git_config_dir"
+mkdir -p "$git_config_dir"
+
+git config --file "$git_config_dir/config" user.name gmarmstrong
+git config --file "$git_config_dir/config" user.email guthrie.armstrong@gmail.com
+git config --file "$git_config_dir/config" core.excludesfile "$HOME/dotfiles/git/gitignore_global"
