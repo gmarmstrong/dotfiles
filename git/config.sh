baea6922f1f01784831f616c15927b2a564d6838
gmarmstrong <guthrie.armstrong@gmail.com>
2017-11-16 21:18:27 -0500

    Enable git diff for GPG

--- git/config.sh
+++ git/config.sh
@@ -10,3 +10,5 @@ mkdir -p "$git_config_dir"
 git config --file "$git_config_dir/config" user.name gmarmstrong
 git config --file "$git_config_dir/config" user.email guthrie.armstrong@gmail.com
 git config --file "$git_config_dir/config" core.excludesFile "$HOME/dotfiles/git/ignore"
+git config --file "$git_config_dir/config" core.attributesFile "$HOME/dotfiles/git/attributes"
+git config --file "$git_config_dir/config" diff.gpg.textconv "gpg --no-tty --decrypt"
