6267055f2ccae9cd292b19f6aa15b499744b38ac
gmarmstrong <guthrie.armstrong@gmail.com>
2017-11-24 23:05:46 -0500

    Document git/config.sh

--- git/config.sh
+++ git/config.sh
@@ -7,6 +7,7 @@ git_config_dir="$HOME/.config/git"
 rm -rf "$git_config_dir"
 mkdir -p "$git_config_dir"
 
+# Generate new git config
 git config --file "$git_config_dir/config" user.name gmarmstrong
 git config --file "$git_config_dir/config" user.email guthrie.armstrong@gmail.com
 git config --file "$git_config_dir/config" core.excludesFile "$HOME/dotfiles/git/ignore"
