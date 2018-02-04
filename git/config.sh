823fd2c5a0141782de8bf7be8cbd64c7b201bebe
gmarmstrong <guthrie.armstrong@gmail.com>
2018-02-04 16:37:34 -0500

    Set gpg.program in gitconfig

--- git/config.sh
+++ git/config.sh
@@ -15,3 +15,4 @@ git config --file "$git_config_dir/config" core.attributesFile "$dotfiles/git/at
 git config --file "$git_config_dir/config" diff.gpg.textconv "gpg --no-tty --decrypt --quiet"
 git config --file "$git_config_dir/config" user.signingkey 100B37EAF2164C8B
 git config --file "$git_config_dir/config" commit.gpgsign true
+git config --file "$git_config_dir/config" gpg.program gpg
