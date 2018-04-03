3aaef6e80d7669ea23bd49ff29972d205f1bd96c
gmarmstrong <guthrie.armstrong@gmail.com>
2018-04-03 14:09:06 -0400

    Reorganize nixos/configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -13,12 +13,10 @@
     allowUnfree = true;
   };
 
-  # Include results of hardware scan
   imports = [
     /etc/nixos/hardware-configuration.nix
   ];
 
-  # Declare startup devices and boot loaders
   boot = {
     initrd.luks.devices = [
       {
@@ -30,11 +28,10 @@
     loader.grub = {
       enable = true;
       version = 2;
-      device = "/dev/sda";
+      device = "/dev/sda"; # TODO Specify device by uuid
     };
   };
 
-  # Declare hardware, including Bluetooth and PulseAudio
   hardware = {
     bluetooth = {
       enable = true;
@@ -45,10 +42,6 @@
     };
   };
 
-  # Environment variables
-  environment.variables.EDITOR = "nvim";
-
-  # Define users (each must set their password with `passwd`)
   users.extraUsers.guthrie = {
     isNormalUser = true;
     home = "/home/guthrie";
@@ -57,155 +50,158 @@
     shell = pkgs.zsh;
   };
 
-  # Networking
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
   };
 
-  # Set your time zone
   time.timeZone = "US/Eastern";
 
-  # GnuPG agent
-  programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
-
-  # Zsh shell
-  programs.zsh.syntaxHighlighting.enable = true;
-  programs.zsh.enableCompletion = true;
-
-  # CUPS for printing documents
-  services.printing.enable = true;
-  services.printing.drivers = [ pkgs.hplip ];
-
-  # Security
-  security.sudo.enable = true;
-
-  ## Physlock (redundant alongside SLiM)
-  services.physlock.enable = true;
-  services.physlock.allowAnyUser = true;
-
-  # X11 window system
-  services.xserver = {
-    autorun = true;
-    serverFlagsSection = # Block tty access from lock screen
-    ''
-      Option "DontVTSwitch" "True"
-    '';
-    displayManager.sessionCommands =
-    ''
-      xrdb -load "$HOME/.config/X11/Xresources" &
-      nm-applet &
-      blueman-applet &
-      pasystray &
-      nextcloud &
-      unclutter -root &
-      . "$HOME/.fehbg" &
-    '';
-    enable = true;
-    exportConfiguration = true;
-    libinput = {
+  programs = {
+    gnupg.agent = {
       enable = true;
-      accelProfile = "flat";
-      tapping = false;
-      tappingDragLock = false;
-      scrollMethod = "edge";
+      enableSSHSupport = true;
     };
-    multitouch.enable = true;
-    windowManager.i3.enable = true;
+    zsh = {
+      syntaxHighlighting.enable = true;
+      enableCompletion = true;
+    };
   };
 
-  # NixOS release version
-  system.stateVersion = "18.03";
+  security.sudo.enable = true;
 
-  # Packages
-  environment.systemPackages = with pkgs; [
-    acpi
-    alsaLib
-    alsaTools
-    alsaUtils
-    androidsdk
-    ardour
-    autoconf
-    automake
-    blueman
-    byzanz
-    calc
-    calibre
-    coreutils
-    curl
-    dict
-    diction
-    feh
-    figlet
-    file
-    firefox
-    firmwareLinuxNonfree
-    ghc
-    git
-    gnome3.gnome_keyring
-    gnumake
-    gnupg
-    gparted
-    htop
-    hugo
-    imagemagick
-    jetbrains.idea-ultimate
-    jetbrains.pycharm-professional
-    json_c
-    libreoffice
-    maven
-    neovim
-    networkmanager.out
-    networkmanager_openvpn
-    networkmanagerapplet
-    nextcloud-client
-    openjdk
-    openssh
-    p7zip
-    pandoc
-    pass
-    pasystray
-    pavucontrol
-    postgresql
-    powerstat
-    powertop
-    psmisc
-    python
-    python3
-    ranger
-    rofi
-    rofi-pass
-    rsync
-    rxvt_unicode_with-plugins
-    scrot
-    signal-desktop
-    slack
-    spotify
-    sshfs
-    stack
-    sxiv
-    texlive.combined.scheme-full
-    thunderbird
-    tor-browser-bundle-bin
-    transmission
-    transmission_gtk
-    trash-cli
-    tree
-    unclutter
-    unzip
-    vlc
-    w3m
-    wget
-    xclip
-    xdg-user-dirs
-    xdg_utils
-    xorg.xbacklight
-    xpdf
-    youtube-dl
-    zathura
-    zip
-    zotero
-    zsh
-  ];
+  services = {
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplip ];
+    };
+    physlock = {
+      enable = true;
+      allowAnyUser = true;
+    };
+    xserver = {
+      autorun = true;
+      serverFlagsSection = # Block tty access from lock screen
+      ''
+        Option "DontVTSwitch" "True"
+      '';
+      displayManager.sessionCommands =
+      ''
+        xrdb -load "$HOME/.config/X11/Xresources" &
+        nm-applet &
+        blueman-applet &
+        pasystray &
+        nextcloud &
+        unclutter -root &
+        . "$HOME/.fehbg" &
+      '';
+      enable = true;
+      exportConfiguration = true;
+      libinput = {
+        enable = true;
+        accelProfile = "flat";
+        tapping = false;
+        tappingDragLock = false;
+        scrollMethod = "edge";
+      };
+      multitouch.enable = true;
+      windowManager.i3.enable = true;
+    };
+  };
+
+  system.stateVersion = "18.03"; # NixOS release version
+
+  environment = {
+    variables.EDITOR = "nvim";
+    systemPackages = with pkgs; [
+      acpi
+      alsaLib
+      alsaTools
+      alsaUtils
+      androidsdk
+      ardour
+      autoconf
+      automake
+      blueman
+      byzanz
+      calc
+      calibre
+      coreutils
+      curl
+      dict
+      diction
+      feh
+      figlet
+      file
+      firefox
+      firmwareLinuxNonfree
+      ghc
+      git
+      gnome3.gnome_keyring
+      gnumake
+      gnupg
+      gparted
+      htop
+      hugo
+      imagemagick
+      jetbrains.idea-ultimate
+      jetbrains.pycharm-professional
+      json_c
+      libreoffice
+      maven
+      neovim
+      networkmanager.out
+      networkmanager_openvpn
+      networkmanagerapplet
+      nextcloud-client
+      openjdk
+      openssh
+      p7zip
+      pandoc
+      pass
+      pasystray
+      pavucontrol
+      postgresql
+      powerstat
+      powertop
+      psmisc
+      python
+      python3
+      ranger
+      rofi
+      rofi-pass
+      rsync
+      rxvt_unicode_with-plugins
+      scrot
+      signal-desktop
+      slack
+      spotify
+      sshfs
+      stack
+      sxiv
+      texlive.combined.scheme-full
+      thunderbird
+      tor-browser-bundle-bin
+      transmission
+      transmission_gtk
+      trash-cli
+      tree
+      unclutter
+      unzip
+      vlc
+      w3m
+      wget
+      xclip
+      xdg-user-dirs
+      xdg_utils
+      xorg.xbacklight
+      xpdf
+      youtube-dl
+      zathura
+      zip
+      zotero
+      zsh
+    ];
+  };
 
 }
