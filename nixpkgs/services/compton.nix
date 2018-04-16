5acf758eda3e5de29564150c6273e2dc299a6868
gmarmstrong <guthrie.armstrong@gmail.com>
2018-04-16 08:03:12 -0400

    Fix compton flickering

--- nixpkgs/services/compton.nix
+++ nixpkgs/services/compton.nix
@@ -4,10 +4,13 @@
   services.compton = {
     enable = false;
     shadow = true;
-    shadowExclude = [ "window_type *= 'menu'" ];
-    fadeExclude = [ "window_type *= 'menu'" ];
+    shadowExclude = [ "window_type *= 'menu'" "class_g = 'Firefox' && argb" ];
+    fadeExclude = [ "window_type *= 'menu'" "class_g = 'Firefox' && argb" ];
     extraOptions = ''
       no-dock-shadow = true;
+      xrender-sync = true;
+      xrender-sync-fence = true;
+      unredir-if-possible = true;
     '';
   };
 }
