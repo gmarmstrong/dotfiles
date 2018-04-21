7c91aa3fca68e31dcd79b3097f1a84dc7888d00b
gmarmstrong <guthrie.armstrong@gmail.com>
2018-04-21 11:34:39 -0400

    Declare gitignore in git.nix

--- nixpkgs/programs/git.nix
+++ nixpkgs/programs/git.nix
@@ -9,16 +9,72 @@
       key = "100B37EAF2164C8B";
       signByDefault = true;
     };
+
     extraConfig = {
-      core = {
-        excludesFile = "${config.home.homeDirectory}/dotfiles/git/ignore";
-        attributesFile = "${config.home.homeDirectory}/dotfiles/git/attributes";
-        editor = "${pkgs.neovim}/bin/nvim";
-      };
-      # FIXME diff.gpg.textconv = "gpg --no-tty --decrypt --quiet";
+      core.editor = "${pkgs.neovim}/bin/nvim";
       commit.gpgsign = true;
       gpg.program = "${pkgs.gnupg}/bin/gpg";
       credential.helper = "cache";
     };
+
+    ignores = [
+      "secring.*"
+      ".ssh/id_rsa"
+      ".ssh/id_rsa.pub"
+      ".gnupg"
+      "*.py[cod]"
+      "*.pdf"
+      "*.aux"
+      "*.lof"
+      "*.log"
+      "*.lot"
+      "*.fls"
+      "*.out"
+      "*.toc"
+      "*.fmt"
+      "*.fot"
+      "*.cb"
+      "*.cb2"
+      "*.dvi"
+      "*-converted-to.*"
+      "*.fdb_latexmk"
+      "*.synctex.gz"
+      "*.bbl"
+      "*.bcf"
+      "*.blg"
+      "*-blx.aux"
+      "*-blx.bib"
+      "*.run.xml"
+      "*.nav"
+      "*.pre"
+      "*.snm"
+      "*.vrb"
+      "*.lol"
+      ".fuse_hidden*"
+      ".Trash-*"
+      ".nfs*"
+      "[._]*.s[a-v][a-z]"
+      "[._]*.sw[a-p]"
+      "[._]s[a-v][a-z]"
+      "[._]sw[a-p]"
+      "Session.vim"
+      ".netrwhist"
+      ".viminfo"
+      "*~"
+      "tags"
+      "*.class"
+      "*.log"
+      "*.ctxt"
+      ".mtj.tmp/"
+      "*.jar"
+      "*.war"
+      "*.ear"
+      "*.zip"
+      "*.tar.gz"
+      "*.rar"
+      "hs_err_pid*"
+      "target/"
+      ".idea/"
+    ];
   };
 }
