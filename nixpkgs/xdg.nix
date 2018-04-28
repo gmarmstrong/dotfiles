da744170e44aee6930534371c1c45aefbba50bfe
gmarmstrong <guthrie.armstrong@gmail.com>
2018-04-28 08:32:31 -0400

    Add filetype associations to ranger

--- nixpkgs/xdg.nix
+++ nixpkgs/xdg.nix
@@ -24,8 +24,15 @@
       rangerRifle = {
         target = "ranger/rifle.conf";
         text = ''
-          ext pdf, has zathura,  X, flag f = zathura -- "$@"
-          ext djvu, has zathura,  X, flag f = zathura -- "$@"
+          ext pdf, has zathura, X, flag f = zathura -- "$@"
+          ext djvu, has zathura, X, flag f = zathura -- "$@"
+          ext epub, has ebook-viewer, X, flag f = ebook-viewer -- "$@"
+          ext mobi, has ebook-viewer, X, flag f = ebook-viewer -- "$@"
+          ext x?html?, has firefox, X, flag f = firefox -- "@"
+          mime ^image, has sxiv, X, flag = sxiv -- "$@"
+          mime ^video|audio, has vlc, X, flag = vlc -- "$@"
+          mime ^text, label editor = "$EDITOR" -- "$@"
+          mime ^text, label pager = "$PAGER" -- "$@"
         '';
       };
 
