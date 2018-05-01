625b9e1d97ba6d4d83a926952f193eac25233f87
gmarmstrong <guthrie.armstrong@gmail.com>
2018-05-01 04:08:34 -0400

    Declutter htop

--- nixpkgs/programs/htop.nix
+++ nixpkgs/programs/htop.nix
@@ -4,7 +4,9 @@
   programs.htop = {
     enable = true;
     fields = [ "PID" "USER" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ];
+    showProgramPath = false;
     hideThreads = true;
+    hideUserlandThreads = true;
     highlightBaseName = true;
   };
 }
