962d06a51038ffb9367aa9f27cb52a1a6683d8ab
gmarmstrong <guthrie.armstrong@gmail.com>
2018-05-02 00:07:55 -0400

    Streamline device mounting

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -56,7 +56,16 @@
 
   time.timeZone = "US/Eastern";
 
-  security.sudo.enable = true;
+  security = {
+    sudo.enable = true;
+    polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+          if ((action.id == "org.freedesktop.udisks2.filesystem-mount-system" ||
+            action.id == "org.freedesktop.udisks2.filesystem-mount") &&
+            subject.isInGroup("wheel")) { return polkit.Result.YES; }
+      });
+    '';
+  };
 
   programs = {
     gnupg.agent.enable = true;
