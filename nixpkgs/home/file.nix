17c3399471ced378a94f32dd4e4dd2b8b01020a6
gmarmstrong <guthrie.armstrong@gmail.com>
2018-06-10 10:53:36 -0400

    Set no-inc-recursive and progress2 for bak

--- nixpkgs/home/file.nix
+++ nixpkgs/home/file.nix
@@ -14,7 +14,7 @@
       target = "${config.home.homeDirectory}/.local/bin/bak";
       text = ''
         #!/usr/bin/env bash
-        sudo rsync -PAXa --delete-excluded \
+        sudo rsync -AXa --delete-excluded --no-inc-recursive --info=progress2 \
             --exclude="${config.xdg.cacheHome}/*" \
             --exclude="${config.xdg.dataHome}/Trash/*" \
             "/home" "/run/media/${config.home.username}/ca3036f2-022d-4b6e-bb03-ed762403fd3b"
