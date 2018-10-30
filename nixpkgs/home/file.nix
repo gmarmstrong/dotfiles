30c1e7b3ad375efc831720ae9fe10806537f281e
gmarmstrong <guthrie.armstrong@gmail.com>
2018-10-30 11:42:00 -0400

    Use udisks without root for drive mounting

--- nixpkgs/home/file.nix
+++ nixpkgs/home/file.nix
@@ -13,10 +13,21 @@
       target = "${config.home.homeDirectory}/.local/bin/bak";
       text = ''
         #!/usr/bin/env bash
-        sudo rsync -AXa --delete-excluded --no-inc-recursive --info=progress2 \
+
+        udisksctl unlock \
+            --block-device /dev/disk/by-uuid/1522b6f4-dd61-490c-884d-7e138f583404 \
+            --key-file <(pass drives/backup | head -n 1 | tr -d '\n')
+        udisksctl mount --block-device /dev/mapper/luks-1522b6f4-dd61-490c-884d-7e138f583404
+        rsync -AXa --delete-excluded --no-inc-recursive --info=progress2 \
             --exclude="${config.xdg.cacheHome}/*" \
             --exclude="${config.xdg.dataHome}/Trash/*" \
-            "/home" "/run/media/${config.home.username}/ca3036f2-022d-4b6e-bb03-ed762403fd3b"
+            "/home" "/run/media/${config.home.username}/456a5ccd-14eb-46be-82db-67ef54655476"
+        udisksctl unmount \
+            --block-device /dev/mapper/luks-1522b6f4-dd61-490c-884d-7e138f583404
+        udisksctl lock \
+            --block-device /dev/disk/by-uuid/1522b6f4-dd61-490c-884d-7e138f583404
+        udisksctl power-off \
+            --block-device /dev/disk/by-uuid/1522b6f4-dd61-490c-884d-7e138f583404
       '';
     };
   };
