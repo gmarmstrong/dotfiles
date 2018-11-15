22bc8da3c91d1adc8718eb56438ec5091d0e3fbe
gmarmstrong <guthrie.armstrong@gmail.com>
2018-11-15 13:19:27 -0500

    Disable compton

--- nixpkgs/config/compton.nix
+++ nixpkgs/config/compton.nix
@@ -2,13 +2,13 @@
 
 {
   services.compton = {
-    enable = true;
+    enable = false;
     shadow = false;
     backend = "xrender";
     shadowExclude = [
       "window_type *= 'menu'"
-      "name = 'polybar-topbar_eDP1'"
-      "name = 'polybar-altbar_HDMI1'"
+      "name = 'polybar-basebar_eDP1'"
+      "name = 'polybar-basebar_HDMI1'"
       "class_g = 'Firefox' && argb"
       "focused = 0"
     ];
@@ -20,8 +20,6 @@
       no-dock-shadow = true;
       xrender-sync = true;
       xrender-sync-fence = true;
-      unredir-if-possible = true;
-      paint-on-overlay = true;
       respect-prop-shadow = true;
       xinerama-shadow-crop = true;
     '';
