701e5506e6138be4ae89eea5375d4f4c57f23f40
gmarmstrong <guthrie.armstrong@gmail.com>
2018-12-03 09:50:24 -0500

    Declare udisksctl script usages

--- nixpkgs/home/file.nix
+++ nixpkgs/home/file.nix
@@ -14,20 +14,23 @@
       text = ''
         #!/usr/bin/env bash
 
-        DEVICE_UUID=1522b6f4-dd61-490c-884d-7e138f583404
-        VOLUME_LABEL=456a5ccd-14eb-46be-82db-67ef54655476
+        device_uuid=1522b6f4-dd61-490c-884d-7e138f583404
+        volume_label=456a5ccd-14eb-46be-82db-67ef54655476
 
-        udisksctl unlock \
-            --block-device /dev/disk/by-uuid/"$DEVICE_UUID" \
-            --key-file <(pass drives/backup | head -n 1 | tr -d '\n')
-        udisksctl mount --block-device /dev/mapper/luks-"$DEVICE_UUID"
+        ${pkgs.udisks}/bin/udisksctl unlock \
+            --block-device /dev/disk/by-uuid/"$device_uuid" \
+            --key-file <("${pkgs.pass}/bin/pass" "drives/backup" | head -n 1 | tr -d '\n') \
+            || exit 1
+        ${pkgs.udisks}/bin/udisksctl mount --block-device /dev/mapper/luks-"$device_uuid" \
+            || exit 1
         ${pkgs.rsync}/bin/rsync -AXa --delete-excluded --no-inc-recursive --info=progress2 \
             --exclude="${config.xdg.cacheHome}/*" \
             --exclude="${config.xdg.dataHome}/Trash/*" \
-            "/home" "/run/media/${config.home.username}/$VOLUME_LABEL"
-        udisksctl unmount --block-device /dev/mapper/luks-"$DEVICE_UUID"
-        udisksctl lock --block-device /dev/disk/by-uuid/"$DEVICE_UUID"
-        udisksctl power-off --block-device /dev/disk/by-uuid/"$DEVICE_UUID"
+            "/home" "/run/media/${config.home.username}/$volume_label" \
+            || exit 1
+        ${pkgs.udisks}/bin/udisksctl unmount --block-device /dev/mapper/luks-"$device_uuid" || exit 1
+        ${pkgs.udisks}/bin/udisksctl lock --block-device /dev/disk/by-uuid/"$device_uuid" || exit 1
+        ${pkgs.udisks}/bin/udisksctl power-off --block-device /dev/disk/by-uuid/"$device_uuid" || exit 1
       '';
     };
   };
