2fe157dee9eda129b9c53106eae56b160eccc567
gmarmstrong <guthrie.armstrong@gmail.com>
2019-03-28 13:16:52 -0400

    Make ranger use local atool

--- nixpkgs/config/ranger.nix
+++ nixpkgs/config/ranger.nix
@@ -13,22 +13,14 @@
   xdg.configFile.rangerRifle = {
     target = "ranger/rifle.conf";
     text = ''
-      ext gpg, label editor = "nvim" -- "$@"
-      ext pdf, has zathura, X, flag f = zathura -- "$@"
-      ext djvu, has zathura, X, flag f = zathura -- "$@"
-      ext epub, has zathura, X, flag f = zathura -- "$@"
+      ext txt|tex|asc|gpg|sh|md|markdown|rst, label editor = nvim -- "$@"
+      ext pdf|djvu|epub, has zathura, X, flag f = zathura -- "$@"
       ext mobi, has ebook-viewer, X, flag f = ebook-viewer -- "$@"
-      ext txt, label editor = nvim -- "$@"
-      ext tex, label editor = nvim -- "$@"
-      ext asc, label editor = nvim -- "$@"
-      ext md, label editor = nvim -- "$@"
-      ext markdown, label editor = nvim -- "$@"
-      ext rst, label editor = nvim -- "$@"
       ext x?html?, has firefox, X, flag f = firefox -- "$@"
-      ext 7z|ace|ar|arc|bz2?|cab|cpio|cpt|deb|dgc|dmg|gz, flag f = "${pkgs.atool}/bin/atool" --extract --each -- "$@"
-      ext iso|jar|msi|pkg|rar|shar|tar|tgz|xar|xpi|xz|zip, flag f = "${pkgs.atool}/bin/atool" --extract --each -- "$@"
+      ext 7z|ace|ar|arc|bz2?|cab|cpio|cpt|deb|dgc|dmg|gz, flag f = atool --extract --each -- "$@"
+      ext iso|jar|msi|pkg|rar|shar|tar|tgz|xar|xpi|xz|zip, flag f = atool --extract --each -- "$@"
       mime ^image, has sxiv, X, flag f = sxiv -a -- "$@"
-      mime ^video|audio, has vlc, X, flag f = "${pkgs.vlc}/bin/vlc" -- "$@"
+      mime ^video|audio, has vlc, X, flag f = vlc -- "$@"
       mime ^text, label editor = nvim -- "$@"
       mime ^text, label pager = "$PAGER" -- "$@"
     '';
