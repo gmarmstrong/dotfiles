e1956826afbb0219715a57a65090dd62de083366
gmarmstrong <guthrie.armstrong@gmail.com>
2019-04-05 13:08:08 -0400

    Enable confirm-paste and nerd fonts

--- nixpkgs/config/xresources.nix
+++ nixpkgs/config/xresources.nix
@@ -13,8 +13,8 @@
       "URxvt.secondaryScreen" = 1;
       "URxvt.secondaryScroll" = 0;
       "URxvt.perl-ext" = "";
-      "URxvt.perl-ext-common" = "font-size";
-      "URxvt.font" = "xft:DejaVu Sans Mono:size=11";
+      "URxvt.perl-ext-common" = "font-size,confirm-paste";
+      "URxvt.font" = "xft:DejaVuSansMono Nerd Font Mono:size=12";
       "URxvt.keysym.C-plus" = "font-size:increase";
       "URxvt.keysym.C-minus" = "font-size:decrease";
       "URxvt.keysym.C-equal" = "font-size:reset";
@@ -28,4 +28,3 @@
     extraConfig = builtins.readFile ( "${config.home.homeDirectory}/dotfiles/resources/base16-xresources/xresources/${config.home.sessionVariables.COLORTHEME}.Xresources" );
   };
 }
-
