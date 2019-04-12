fbe4677d7528e895206d6ccd3e2d6fdf5cc377cb
gmarmstrong <guthrie.armstrong@gmail.com>
2019-04-12 16:37:13 -0400

    Update packages list

--- nixpkgs/home/packages.nix
+++ nixpkgs/home/packages.nix
@@ -3,17 +3,6 @@
 {
   home.packages = with pkgs; [
 
-    # Trying:
-    qutebrowser # vi-like web browser (GUI)
-    sqlitebrowser
-    audacity
-    firefox
-    (lowPrio firefox-devedition-bin)
-    wireshark
-    gksu
-    maven3
-    gnumake
-
     # Considering:
     # passExtensions.pass-audit   # pass: auditing command
     # passExtensions.pass-update  # pass: updating command
@@ -26,7 +15,7 @@
     # Waiting for:
     # bitwarden-cli # NixOS/nixpkgs: #51212 #53876
     # bitwarden     # (GUI) # NixOS/nixpkgs: #51212
-    # jabref-4      # (GUI) NixOS/nixpkgs: #47113
+    # jabref        # (GUI) NixOS/nixpkgs: #47113
     # passExtensions.pass-tomb # zeapo/Android-Password-Store: #329
 
     # System requirements (TODO nixify)
@@ -43,47 +32,50 @@
     dos2unix  # line break converter
     file      # file info
     inetutils # network utilities
-    p7zip # 7-Zip archive tools
     psmisc lsof # process utilities
     qrencode # QR code encoder
     trash-cli # trash can
     tree # depth-indented directory listing
-    unrar # rar archive tools
     usbutils # USB device tools
     curl wget youtube-dl # downloaders
     xorg.xev # X event monitor
     xorg.xwininfo # X window info
-    zip unzip # zip archive tools
+    zip unzip p7zip unrar # archive tools
 
     # Development
-    adoptopenjdk-hotspot-bin-11
+    adoptopenjdk-hotspot-bin-11 # prebuilt openjdk binary
     jetbrains.idea-ultimate # IDE (GUI) (UNFREE)
-    python3 # programming language (TODO development dependency)
+    maven3 # java build tool
+    python3 # TODO development dependency
     universal-ctags # source code indexer (TODO Vim tagbar dependency)
+    coq # proof assistant (GUI) # TODO nixify projects
+    # gnumake
     # rustc cargo # TODO nixify Rust projects
     # gnumake # TODO nixify projects
     # nixops # TODO nixify NixOps projects
     # jdk8 # TODO nixify Java projects
 
     # Security
-    coq # proof assistant (GUI) # TODO nixify projects
     gnupg # openpgp
     pass # password manager
     tomb # storage file encryption
+    gksu # su frontend (GUI)
 
     # Textual applications
-    dict # dictionary client
     diction # english linter
     libqalculate # calculator
     ranger # file manager
+    hledger
 
     # Graphical applications
     anki  # flashcards (GUI)
     calibre # e-book manager (GUI)
+    firefox
     hexchat # IRC client (GUI)
     insomnia # REST API client (GUI)
     qalculate-gtk # calculator (GUI)
     qbittorrent # bittorrent client (GUI)
+    qutebrowser # vi-like web browser (GUI)
     signal-desktop # messaging client (GUI)
     skype # VoIP client (UNFREE) (GUI)
     slack # chat client (GUI) (UNFREE)
@@ -93,13 +85,13 @@
     zotero # reference management (GUI)
 
     # General:
+    audacity
     byzanz scrot # screen capture
     gparted # disk partitioner (GUI)
     imagemagick # bitmap image editor
     man-pages # linux man-pages
     mediainfo # multimedia info
     mkvtoolnix # matroska tools (GUI)
-    nextcloud-client # cloud storage client (GUI)
     pandoc # markup converter
     rxvt_unicode-with-plugins # terminal emulator
     sshfs # remote filesystem mounter
@@ -108,6 +100,6 @@
     xclip xsel # X clipboard console support
     xdg-user-dirs
     xdg_utils
-    xxd
+    xxd # create or reverse hexdumps
   ];
 }
