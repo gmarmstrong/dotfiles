fbc3b6ba8c09a64a11e278160634c6303aa29972
gmarmstrong <guthrie.armstrong@gmail.com>
2019-10-03 12:10:01 -0400

    Add paths to bak ignore

--- nixpkgs/home/file.nix
+++ nixpkgs/home/file.nix
@@ -26,6 +26,10 @@
         ${pkgs.rsync}/bin/rsync -AXa --delete-excluded --no-inc-recursive --info=progress2 \
             --exclude="${config.xdg.cacheHome}/*" \
             --exclude="${config.xdg.dataHome}/Trash/*" \
+            --exclude="${config.xdg.dataHome}/android/*" \
+            --exclude="${config.home.homeDirectory}/.android/*" \
+            --exclude="${config.home.homeDirectory}/virtualbox/*" \
+            --exclude="${config.home.homeDirectory}/media/*" \
             "/home" "/run/media/${config.home.username}/$volume_label" \
             || exit 1
         ${pkgs.udisks}/bin/udisksctl unmount --block-device /dev/mapper/luks-"$device_uuid" || exit 1
