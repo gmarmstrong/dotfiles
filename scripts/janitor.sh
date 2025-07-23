3a424c6837d48db194261eb458c00e69ef03901e
Guthrie McAfee Armstrong <guthrie.armstrong@coalitioninc.com>
2025-07-23 12:40:10 -0400

    run flake update as root, pretty-print janitor logging

--- scripts/janitor.sh
+++ scripts/janitor.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
-set -u
+set -o errexit
+set -o nounset
+set -o pipefail
 
 # Update dotfiles
+printf "\n\033[1m>> Updating dotfiles...\033[0m\n\n"
 git -C "$HOME/dotfiles" pull
 
 # Update flake inputs
-nix flake update --flake "$HOME/.config/nix"
+printf "\n\033[1m>> Updating config flake inputs...\033[0m\n\n"
+sudo nix flake update --flake "$HOME/.config/nix"
 
 # Rebuild and apply config flake
+printf "\n\033[1m>> Rebuilding and applying config flake...\033[0m\n\n"
 sudo darwin-rebuild switch --flake "$HOME/.config/nix#mbp"
 
