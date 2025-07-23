af5cd99653b76ee68fdd8d59da7ab320c80205ca
Guthrie McAfee Armstrong <guthrie.armstrong@coalitioninc.com>
2025-07-23 12:30:41 -0400

    run flake update non-root

--- scripts/janitor.sh
+++ scripts/janitor.sh
@@ -6,7 +6,7 @@ set -u
 git -C "$HOME/dotfiles" pull
 
 # Update flake inputs
-sudo nix flake update --flake "$HOME/.config/nix"
+nix flake update --flake "$HOME/.config/nix"
 
 # Rebuild and apply config flake
 sudo darwin-rebuild switch --flake "$HOME/.config/nix#mbp"
