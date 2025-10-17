846b7e955f4a66f0027876e9e57d623d35077d00
Guthrie McAfee Armstrong <guthrie.armstrong@coalitioninc.com>
2025-10-17 15:18:58 -0500

    add nix cleanup step to janitor.sh

--- scripts/janitor.sh
+++ scripts/janitor.sh
@@ -16,3 +16,8 @@ nix flake update --flake "$HOME/.config/nix"
 printf "\n\033[1m>> Rebuilding and applying config flake...\033[0m\n\n"
 sudo darwin-rebuild switch --flake "$HOME/.config/nix#101206-F724N5WGX2"
 
+# Nix garbage collection and store optimization
+printf "\n\033[1m>> Running nix garbage collection...\033[0m\n\n"
+nix-collect-garbage --delete-generations +5
+printf "\n\033[1m>> Optimizing nix store...\033[0m\n\n"
+nix store optimise
