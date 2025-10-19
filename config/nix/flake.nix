5b67bced224013a373f9ef9a5925ca5e203fcc5d
Guthrie McAfee Armstrong <guthrie.armstrong@coalitioninc.com>
2025-10-19 10:09:45 -0500

    config: install nixpkgs-fmt to packages

--- config/nix/flake.nix
+++ config/nix/flake.nix
@@ -69,6 +69,7 @@
         pkgs.go
         pkgs.golangci-lint
         pkgs.jq
+        pkgs.nixpkgs-fmt
         pkgs.ollama
         pkgs.shellcheck
         pkgs.tree
@@ -151,9 +152,6 @@
 
   in
   {
-    # Formatter for `nix fmt`
-    formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
-
     # Work computer
     darwinConfigurations."101206-F724N5WGX2" = nix-darwin.lib.darwinSystem {
       system = "aarch64-darwin";
