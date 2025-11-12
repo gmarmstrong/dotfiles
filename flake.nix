3c79b116c67ffba84fb24cacf5aadea5c9eeabbb
Guthrie McAfee Armstrong <guthrie.armstrong@coalitioninc.com>
2025-11-12 10:00:53 -0500

    refactor: add multi-system support for checks and formatter

--- flake.nix
+++ flake.nix
@@ -24,65 +24,70 @@
 
       # Import host configurations
       workMacbookConfig = import ./hosts/work-macbook;
+
+      # Supported systems for checks and formatters
+      supportedSystems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      # Helper to generate attributes for each system
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Helper to get nixpkgs for a system
+      nixpkgsFor = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
       # Work computer
       darwinConfigurations.${workMacbookConfig.hostname} = mkDarwinSystem workMacbookConfig;
 
-      # Validation checks
-      checks.aarch64-darwin = {
-        work-macbook = self.darwinConfigurations.${workMacbookConfig.hostname}.system;
+      # Validation checks for all systems
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          statix = pkgs.runCommand "statix-check" { nativeBuildInputs = [ pkgs.statix ]; } ''
+            cd ${self}
+            statix check .
+            touch $out
+          '';
 
-        statix =
-          nixpkgs.legacyPackages.aarch64-darwin.runCommand "statix-check"
-            {
-              nativeBuildInputs = [ nixpkgs.legacyPackages.aarch64-darwin.statix ];
-            }
-            ''
-              cd ${self}
-              statix check .
-              touch $out
-            '';
+          deadnix = pkgs.runCommand "deadnix-check" { nativeBuildInputs = [ pkgs.deadnix ]; } ''
+            cd ${self}
+            deadnix --fail .
+            touch $out
+          '';
 
-        deadnix =
-          nixpkgs.legacyPackages.aarch64-darwin.runCommand "deadnix-check"
-            {
-              nativeBuildInputs = [ nixpkgs.legacyPackages.aarch64-darwin.deadnix ];
-            }
-            ''
-              cd ${self}
-              deadnix --fail .
-              touch $out
-            '';
+          shellcheck = pkgs.runCommand "shellcheck-check" { nativeBuildInputs = [ pkgs.shellcheck ]; } ''
+            cd ${self}
+            # Check all bash scripts in scripts/ and .githooks/
+            find scripts/ .githooks/ -type f \( -executable -o -name '*.sh' \) | while read -r script; do
+              echo "Checking $script..."
+              shellcheck "$script"
+            done
+            touch $out
+          '';
 
-        shellcheck =
-          nixpkgs.legacyPackages.aarch64-darwin.runCommand "shellcheck-check"
-            {
-              nativeBuildInputs = [ nixpkgs.legacyPackages.aarch64-darwin.shellcheck ];
-            }
-            ''
-              cd ${self}
-              # Check all bash scripts in scripts/ and .githooks/
-              find scripts/ .githooks/ -type f \( -executable -o -name '*.sh' \) | while read -r script; do
-                echo "Checking $script..."
-                shellcheck "$script"
-              done
-              touch $out
-            '';
-
-        formatting =
-          nixpkgs.legacyPackages.aarch64-darwin.runCommand "formatting-check"
-            {
-              nativeBuildInputs = [ nixpkgs.legacyPackages.aarch64-darwin.nixfmt-tree ];
-            }
-            ''
-              cd ${self}
-              treefmt --fail-on-change --no-cache
-              touch $out
-            '';
-      };
+          formatting =
+            pkgs.runCommand "formatting-check" { nativeBuildInputs = [ pkgs.nixfmt-rfc-style ]; }
+              ''
+                cd ${self}
+                # Format all .nix files and check for changes
+                find . -name '*.nix' -type f -exec nixfmt --check {} +
+                touch $out
+              '';
+        }
+        # Only check darwin configurations on darwin systems
+        // nixpkgs.lib.optionalAttrs (nixpkgs.lib.hasSuffix "darwin" system) {
+          work-macbook = self.darwinConfigurations.${workMacbookConfig.hostname}.system;
+        }
+      );
 
       # Formatter for `nix fmt`
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-tree;
+      formatter = forAllSystems (system: nixpkgsFor.${system}.nixfmt-rfc-style);
     };
 }
