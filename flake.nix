5a693f23c5e43cc8512b2603ae9dcd289e5feae9
Guthrie McAfee Armstrong <git@gmarmstrong.dev>
2026-08-18 20:38:44 -0400

    chore(deps): upgrade Nix inputs to 26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "Modular system configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
